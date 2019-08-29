d449b271481f24322fe9e29b3b10dbf47a8ea468
Ming Gu <migu@microsoft.com>
2019-08-29 16:50:08 +0800

    Add training package which save model using sdk.

---
+++ azureml-custom-module-examples/densenet-img-cls-v2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='densenetimgcls_v2',
+    version='0.0.1',
+    description='DenseNet Model for Image Classification V2',
+    packages=find_packages(),
+    author='Yuhang Zhao',
+    license='MIT',
+    include_package_data=True,
+)
